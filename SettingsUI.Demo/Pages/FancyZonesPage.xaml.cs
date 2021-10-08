363e252adcdda6e8ab443b30670b13adea9637fe
Mahdi Hosseini <mahdidvb72@gmail.com>
2021-10-08 13:26:24 +0330

    Fix Demo Missing Pages

---
+++ SettingsUI.Demo/Pages/FancyZonesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace SettingsUI.Demo.Pages
+{
+    public sealed partial class FancyZonesPage : Page
+    {
+        public FancyZonesPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
